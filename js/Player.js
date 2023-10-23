f05b1bdb62b73108d0d9273ffe099abdc2d17010
cristinamedrom <cristinamedranoroman@gmail.com>
2023-10-23 20:36:21 +0200

    Clase de Player y attackPlayer

--- js/Player.js
+++ js/Player.js
@@ -1,1 +1,11 @@
-class Player {}
+class Player {
+    constructor(name, life, attack) {
+        this.name = name;
+        this.life = life;
+        this.attack = attack;
+    }
+
+    attackPlayer(player, diceValue) {
+        player.life -= (this.attack * diceValue);
+    }
+}
